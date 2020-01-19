facc2edb6b1f8d094265a16ca07f9848bf1da8bc
nschantz21 <nschantz21@gmail.com>
2020-01-19 02:44:03 +0000

    fixed some errors in Homework1

--- Homework1.java
+++ Homework1.java
@@ -2,10 +2,10 @@ import java.time.LocalDateTime;
 import java.util.Random;
 import java.io.*;
 
-class Homework1() {
+public class Homework1 {
     static int[] insert(int array[], int index, int value) {
         // create new array one larger than original array
-        newArray = new int[array.length + 1];
+        int[] newArray = new int[array.length + 1];
         // copy elements up to insert point from original array to new array
         for (int curr_index = 0; curr_index < index; curr_index++) {
             newArray[curr_index] = array[curr_index];
@@ -13,17 +13,17 @@ class Homework1() {
         // place insert value into new array
         newArray[index] = value;
         // copy elements after insert point from original array to new array
-        for (int sec_indx = index; sec_indx < array.length; sec_index++) {
-            newArray[sec_index + 1] = array[sec_index];
+        for (int sec_idx = index; sec_idx < array.length; sec_idx++) {
+            newArray[sec_idx + 1] = array[sec_idx];
         }
         return newArray;
     }
 
-    int main() {
-        NUM_READINGS = 60;
-        INSERTS_PER_READING = 1000;
+    public static void main(String[] args) {
+        int NUM_READINGS = 60;
+        int INSERTS_PER_READING = 1000;
 
-        array = new int[1];
+        int[] array = new int[1];
         Random random = new Random(); // set random seed
 
         // print table headers
@@ -33,20 +33,21 @@ class Homework1() {
         for (int cnt = 0; cnt < NUM_READINGS; cnt++) {
             
             // each reading will be taken adter INSERTS_PER_READING inserts
-            start_time = LocalDateTime.now(); // current time
+            long startTime = System.currentTimeMillis(); // current time
             
             for (int insrts = 0; insrts < INSERTS_PER_READING; insrts++) {
                 int index = random.nextInt(array.length);
                 int value = random.nextInt();
-                int array = Homework1(array, index, value);
+                array = Homework1.insert(array, index, value);
             }
 
-            stopTime = LocalDateTime.now();
-            timePerInsert = (stopTime - startTime) / INSERTS_PER_READING;
+            long stopTime = System.currentTimeMillis();
+            float timePerInsert = (stopTime - startTime) / INSERTS_PER_READING;
             
             // output reading in tabular format
             //System.out.println(array.length + " " + timePerInsert);
-            System.out.format("%12d %12.6f\n", array.length, timePerInsert);
+            System.out.format("%12d %21.10f\n", array.length, timePerInsert);
         }
+        return;
     }
 }
